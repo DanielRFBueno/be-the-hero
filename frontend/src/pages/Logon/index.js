b32c777d13ce7e38015b310477d799cd7929cb13
DanielRFBueno <danielonsoftwarelabs@gmail.com>
2020-03-26 17:33:01 -0300

    Finished the Register page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {FiLogIn} from "react-icons/fi";
+import {Link} from 'react-router-dom';
 
 import './styles.css';
 
@@ -21,11 +22,12 @@ export default function Logon() {
                             type="submit">Entrar
                     </button>
 
-                    <a href="/register">
+                    <Link className="back-link"
+                          to="/register">
                         <FiLogIn size={16}
                                  color="E02041"/>
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
